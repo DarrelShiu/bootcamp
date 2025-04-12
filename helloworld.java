7d33e0307d0b2d449e71f45a6e1c303f8cd9492a
darrel.shiu <darrelshiu@gmail.com>
2025-04-12 10:38:34 +0800

    bug fixed 20250412 3.0

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,6 @@
-package test;
 public class helloworld {
   public static void main(String[] args) {
     // I love java
-    System.out.println("hello_hello1");
+    System.out.println("hello_hello2");
   }
 }
