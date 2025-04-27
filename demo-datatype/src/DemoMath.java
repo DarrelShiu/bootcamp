1513d3aede418eb7701ba4fdc7ac0497d52e46d1
darrel.shiu <darrelshiu@gmail.com>
2025-04-27 09:22:29 +0800

    HomeWork27042025

---
+++ demo-datatype/src/DemoMath.java
@@ -0,0 +1,22 @@
+package src;
+
+public class DemoMath {
+  public static void main(String[] args) {
+    // pow
+    // 2^4 = 16
+    double result = Math.pow(2.0, 4.0);
+    System.out.println(result);
+    double result1 = Math.pow(2.0, 1.0);
+    System.out.println(result1);
+
+    int x = 3;
+    int y = 7;
+    System.out.println(Math.abs(x - y));
+
+    System.out.println(Math.round(9.45));
+    System.out.println(Math.sqrt(81));
+
+    System.out.println(Math.max(3, 9.0));
+
+  }
+}
